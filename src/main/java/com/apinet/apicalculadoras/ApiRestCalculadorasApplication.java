be4f8c28371c9c1e5e6e7fd6dca9b3b9b6c69b20
saulocapistrano <saulocapistrano@gmail.com>
2022-09-11 10:55:23 -0300

    Criação do Projeto

---
+++ src/main/java/com/apinet/apicalculadoras/ApiRestCalculadorasApplication.java
@@ -0,0 +1,13 @@
+package com.apinet.apicalculadoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCalculadorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCalculadorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apinet/apicalculadoras/ApiRestCalculadorasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apinet.apicalculadoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCalculadorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
